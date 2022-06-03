229b21ca664029bced7284d56499171c5a69f0f7
luke <luke@smartcow.ai>
2022-06-03 17:34:13 +0200

    New Feature: Added Diarizer Code

--- src/diarization.py
+++ src/diarization.py
@@ -7,10 +7,12 @@ class Diarizer:
     def _diarize(self, input_file):
         diarization = self.pipeline(input_file)
         speakers = []
-        for turn, _, speaker in diarization.itertracks(yield_label=True):
-            speakers.append([turn.start, turn.end, speaker]) # start time, end time, speaker
-        return speakers
 
+        for turn, _, speaker in diarization.itertracks(yield_label=True):
+            speakers.append([turn.start*1000, turn.end*1000, speaker]) # start time, end time, speaker
+
+        return speakers
+        
 if __name__ == "__main__":
-    speakers = Diarizer()._diarize("inputs/test.wav")
+    speakers = Diarizer()._diarize("inputs_processed/test.wav")
     print(speakers)
---
+++ src/punctuation.py
@@ -0,0 +1,1 @@
+## Include model to restore punctuation to sentences
