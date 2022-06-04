dbb733f62b1931ce0dc09d22c5b3e225866d0704
lukeabela38 <lukeabela56@gmail.com>
2022-06-04 16:56:58 +0200

    New Feature: Added Punctuation

--- src/punctuation.py
+++ src/punctuation.py
@@ -1,1 +1,11 @@
-## Include model to restore punctuation to sentences
+import requests
+
+def punctuate(text):
+    data = {'text': text,}
+    response = requests.post('http://bark.phon.ioc.ee/punctuator', data=data)
+    return response.text
+
+if __name__ == "__main__":
+
+    response = punctuate("will you follow me to the moon")
+    print(response)
